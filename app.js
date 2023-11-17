5cd0dc1cb4c995d1ca2831d456fbecb0ede6f33a
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-17 20:19:55 +0500

    working on UI

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ function setReminder() {
     alert("Please enter a valid time for the reminder.");
     return;
   }
- []
+ 
   // Parse the time and convert it to milliseconds
   const milliseconds = new Date(reminderTime) - new Date();
-  console.log(new Date(reminderTime))
-  console.log(new Date())
+  console.log(new Date(reminderTime));
+  console.log(new Date());
+  console.log(milliseconds);
 
   // Set a timeout to show the notification
   setTimeout(() => {
@@ -23,22 +24,21 @@ function showNotification() {
   // Check if the Notification API is available
   if (!("Notification" in window)) {
     alert("This browser does not support desktop notification");
-  } else if (Notification.permission === "defualt") {
+  } else if (Notification.permission === "permitted") {
     // If permission is already granted, show the notification
     new Notification("Reminder", {
-      body: "It's time for your reminder!",
+      body: "ALERT!!! It's time for your reminder!",
     });
   } else if (Notification.permission !== "denied") {
     // Request permission from the user
     Notification.requestPermission().then(function (permission) {
-      if (permission === "granted") {
+      if (permission === "default") {
         // If the user allows, show the notification
         new Notification("Reminder", {
-          body: "It's time for your reminder!",
+          body: "granted It's time for your reminder!",
         });
       }
     });
   }
 }
 
-// console.log(window);
