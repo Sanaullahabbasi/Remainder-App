6668ea2f22151178f84ca0fee3cf21f85fd55136
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-19 22:10:53 +0500

    hmtl and js file merge

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let ringtone = new Audio("audio/ringtone.mp3");
 function setReminder() {
   // Get user input for reminder time
   const reminderTime = document.getElementById("reminderTime").value;
-console.log(reminderTime);
   // Validate the input
   if (!reminderTime) {
     alert("Please enter a valid time for the reminder.");
@@ -42,4 +41,69 @@ function showNotification() {
       }
     });
   }
+}
+
+
+
+// **** Dashboard Javascript programming ****
+let day = document.getElementById("day"),
+  time = document.getElementById("time"),
+  currentDate = new Date();
+
+let timeInterval;
+
+//**** get day of the week ****//
+
+let daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thurday",
+  "Friday",
+  "Saturday",
+];
+let flag = false,
+  today = "";
+for (var i = 0; i < daysOfWeek.length; i++) {
+  if (i === currentDate.getDay()) {
+    flag = true;
+    today = daysOfWeek[i];
+  }
+}
+
+if (flag) {
+  day.innerHTML = `${today}`;
+} else {
+  day.innerHTML = `today`;
+}
+
+// **** get current time **** //
+
+function msToTime() {
+  let currentTime = new Date(),
+    seconds = Math.floor(currentTime.getSeconds()),
+    minutes = Math.floor(currentTime.getMinutes()),
+    hours = Math.floor(currentTime.getHours()),
+    ampm;
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  ampm = hours > 12 ? "PM" : "AM";
+  hours = hours > 12 ? hours - 12 : hours;
+
+  return hours + ":" + minutes + ":" + seconds + " " + ampm;
+}
+timeInterval = setInterval(function () {
+  time.innerHTML = msToTime();
+}, 10);
+
+// *****  adding reminder setting ***** 
+let mainUI = document.getElementById("container"),
+addRemUI = document.querySelector(".main");
+function addRem (){
+  addRemUI.style.display= "block";
+  addRemUI.style.display= "flex";
+mainUI.style.display = "none";
 }
--- toDo.js
+++ toDo.js
@@ -1,55 +1,53 @@
-// **** Dashboard Javascript programming ****
-const remUI = document.getElementById("add_rem");
+// // **** Dashboard Javascript programming ****
+// let day = document.getElementById("day"),
+//   time = document.getElementById("time"),
+//   currentDate = new Date();
 
-let day = document.getElementById("day"),
-  time = document.getElementById("time"),
-  currentDate = new Date();
+// let timeInterval;
 
-let timeInterval;
+// //**** get day of the week ****//
 
-//**** get day of the week ****//
+// let daysOfWeek = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Wednesday",
+//   "Thurday",
+//   "Friday",
+//   "Saturday",
+// ];
+// let flag = false,
+//   today = "";
+// for (var i = 0; i < daysOfWeek.length; i++) {
+//   if (i === currentDate.getDay()) {
+//     flag = true;
+//     today = daysOfWeek[i];
+//   }
+// }
 
-let daysOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thurday",
-  "Friday",
-  "Saturday",
-];
-let flag = false,
-  today = "";
-for (var i = 0; i < daysOfWeek.length; i++) {
-  if (i === currentDate.getDay()) {
-    flag = true;
-    today = daysOfWeek[i];
-  }
-}
+// if (flag) {
+//   day.innerHTML = today;
+// } else {
+//   day.innerHTML = `today`;
+// }
 
-if (flag) {
-  day.innerHTML = today;
-} else {
-  day.innerHTML = `today`;
-}
+// // **** get current time **** //
 
-// **** get current time **** //
+// function msToTime() {
+//   let currentTime = new Date(),
+//     seconds = Math.floor(currentTime.getSeconds()),
+//     minutes = Math.floor(currentTime.getMinutes()),
+//     hours = Math.floor(currentTime.getHours()),
+//     ampm;
 
-function msToTime() {
-  let currentTime = new Date(),
-    seconds = Math.floor(currentTime.getSeconds()),
-    minutes = Math.floor(currentTime.getMinutes()),
-    hours = Math.floor(currentTime.getHours()),
-    ampm;
+//   hours = hours < 10 ? "0" + hours : hours;
+//   minutes = minutes < 10 ? "0" + minutes : minutes;
+//   seconds = seconds < 10 ? "0" + seconds : seconds;
+//   ampm = hours > 12 ? "PM" : "AM";
+//   hours = hours > 12 ? hours - 12 : hours;
 
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-  ampm = hours > 12 ? "PM" : "AM";
-  hours = hours > 12 ? hours - 12 : hours;
-
-  return hours + ":" + minutes + ":" + seconds + " " + ampm;
-}
-timeInterval = setInterval(function () {
-  time.innerHTML = `${msToTime()}`;
-}, 10);
+//   return hours + ":" + minutes + ":" + seconds + " " + ampm;
+// }
+// timeInterval = setInterval(function () {
+//   time.innerHTML = `${msToTime()}`;
+// }, 10);
